f26e87a2cca4cbe4411b9a6bcf4f65c3fae4f14f
Julia <alicerenard86@gmail.com>
2019-04-24 23:58:41 +0700

    New sort function.

--- src/components/ContactList/index.js
+++ src/components/ContactList/index.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {MDBIcon} from 'mdbreact';
 import Contact from '../Contact'
 import './style.css'
 
 
-export default function ContactList( {contacts, onContactsChanged} ) {
-    const contactElements = contacts.map(contact =>
-          <li key = {contact.username} className="contact-list_li"><Contact contact = {contact} onContactsChanged = {onContactsChanged}/></li>)
+
+class ContactList extends Component {
+  constructor(props) {
+    super(props)
+
+    this.contacts = props.contacts.slice()
+    
+    this.state = {}
+  }    
+  
+  render() {
+    const {onContactsChanged} = this.props
+
+    const contactElements = this.contacts.map(contact =>
+      <li key = {contact.username} className="contact-list_li"><Contact contact = {contact} onContactsChanged = {onContactsChanged}/></li>)
+
     return (       
-         <ul>
-            {contactElements}
-         </ul>
+      <div> 
+        <div className="text-center">
+          <MDBIcon icon="angle-double-up" size="2x" onClick={this.sortUpClick}/> &nbsp;            
+          <MDBIcon icon="angle-double-down" size="2x" onClick={this.sortDownClick}/>              
+        </div>  
+        <ul>
+          {contactElements}
+        </ul>
+      </div>  
     )
+  }
+
+  sortUpClick = () => {
+    this.contacts.sort((a, b) => a.name.localeCompare(b.name))
+    
+    this.setState((state) => { return {} })
+  }
+
+  sortDownClick = () => {
+    this.contacts.sort((b, a) => a.name.localeCompare(b.name))
+    
+    this.setState((state) => { return {} })
+  }
 }
+
+export default ContactList
